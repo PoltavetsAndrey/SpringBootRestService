3c3fede7cb99254275e587a1dc72eb2995eab1cd
PoltavetsAndrey <POLtavetsandrey83@icloud.com>
2022-01-14 01:12:34 +0200

    video 1 hour 26 minutes

--- practika-29-2/src/main/java/AuthorDao.java
+++ practika-29-2/src/main/java/AuthorDao.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
@@ -28,7 +25,7 @@ public class AuthorDao {
         try (Statement statement = connection.createStatement()) {
             ResultSet cursor = statement.executeQuery("SELECT * FROM author");
             if (cursor.next()) {
-                return (authors.add(createAuthorFromCursorIfPossible(cursor));
+                authors.add(createAuthorFromCursorIfPossible(cursor));
             }
         }
         return authors;
@@ -40,7 +37,7 @@ public class AuthorDao {
             ResultSet cursor = statement.executeQuery(String.format(
                     "SELECT * FROM author WHERE id = %d", id));
             if (cursor.next()) {
-                return Optional.of(authors.add(createAuthorFromCursorIfPossible(cursor));
+                return Optional.of(authors.add(createAuthorFromCursorIfPossible(cursor)));
             } else {
                 return Optional.empty();
             }
@@ -50,9 +47,10 @@ public class AuthorDao {
     public Collection<Author> findByName() throws SQLException {
         Collection<Author> authors = new ArrayList<>();
         try (Statement statement = connection.createStatement()) {
-            ResultSet cursor = statement.executeQuery("SELECT * FROM author WHERE NAME LIKE '%s', text");
+            ResultSet cursor = statement.executeQuery("SELECT * FROM author " +
+                    "WHERE NAME LIKE '%%%s%%', text");
             if (cursor.next()) {
-                return (authors.add(createAuthorFromCursorIfPossible(cursor));
+                authors.add(createAuthorFromCursorIfPossible(cursor));
             }
         }
         return authors;
@@ -60,5 +58,40 @@ public class AuthorDao {
 
     private Author createAuthorFromCursorIfPossible(ResultSet cursor) throws SQLException {
         Author author = new Author();
+        author.id = cursor.getInt("id");
+        author.name = cursor.getString("name");
+        author.birthYear = cursor.getInt("birth_year");
+        return author;
+    }
+
+    public void update(Author author) throws SQLException {
+        String sql = "UPDATE author SET name = ?, birth_year = ?" +
+                "WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setString(1, author.name);
+            statement.setInt(2, author.birthYear);
+            statement.setInt(3, author.id);
+
+            statement.executeUpdate();
+                    String.format("SELECT FROM author" +
+                            "WHERE name LIKE '%%%s%%' ", text));
+            while (cursor.next()) {
+
+            }
+            )
+        }
+    }
+
+    public void insert(Author author) throws SQLException {
+        if (author.id != 0) {
+            throw new IllegalArgumentException("ID is: ", author.id);
+        }
+        final String sql = "INSERT INTO author (name, birth_year) VALUES (?,?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, author.name);
+            statement.setInt(2, author.birthYear);
+            statement.executeUpdate();
+        }
+
     }
 }
